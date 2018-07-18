500f02a1e1767583555d9fe639bb56f7f8285cff
一个疯狂的前端学生 <37667296+hqx1223614734@users.noreply.github.com>
2018-07-18 11:30:29 +0800

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -26,9 +26,7 @@ export default new vuex.Store({
   mutations: {
     addGroupMsg(state, msg){
       state.groupMsg.push(msg);
-      if(state.groupMsg.length >= maxNum){
-        state.groupMsg.splice(0, state.groupMsg.length - maxNum);
-      }
+      deleteMoreMsg(state.groupMsg, state.historyNum);
     },
     persons(state, p){
       state.onlinePersons = p;
@@ -59,80 +57,96 @@ export default new vuex.Store({
     },
     changeName(state, data){
       state.groupMsg.forEach(item => {
-        if(item.id === data.id){
+        if(item.account === data.account){
           item.name = data.name;
         }
       });
-      if(state.id === data.id){
+      if(state.account === data.account){
         for(let i in state.msgWith){
           state.msgWith[i].forEach(item => {
-            if(item.fromId === data.id){
+            if(item.account === data.account){
               item.name = data.name;
             }
           })
         }
       }else{
         state.nearMsg.forEach(item => {
-          if(item.id === data.id){
+          if(item.account === data.account){
             item.name = data.name;
           }
         });
-        if(!state.msgWith[data.id])return;
-        state.msgWith[data.id].forEach(item => {
-          if(item.fromId === data.id){
+        if(!state.msgWith[data.account])return;
+        state.msgWith[data.account].forEach(item => {
+          if(item.account === data.account){
             item.name = data.name;
           }
         })
       }
     },
-    setChatWithWho(state, id){
-      state.chatWithWho = id;
+    setChatWithWho(state, account){
+      state.chatWithWho = account;
+      let item = state.nearMsg.find(item => item.account === account);
+      if(item){
+        item.noViewMsgNum = 0;
+      }
     },
     oneToOneChat(state, data){
-      let obj = state.msgWith[data.fromId],
-          indexInNear = state.nearMsg.findIndex(item => item.id === data.fromId);
+      let obj = state.msgWith[data.account],
+          indexInNear = state.nearMsg.findIndex(item => item.account === data.account);
       if(indexInNear === -1){
         indexInNear = state.nearMsg.length;
         state.nearMsg.push({
           id: data.fromId,
           headUrl: data.headUrl,
           name: data.name,
-          noViewMsgNum: 1
+          noViewMsgNum: 1,
+          account: data.account
         });
-      }
-      if(obj === undefined){
-        obj = state.msgWith[data.fromId] = [];
-      }
-      obj.push(data);
-      //state.msgWith[data.fromId] = [].concat(obj);
-      bus.$emit('update');
-      if(state.chatWithWho !== data.fromId) {
+      }else if(state.account !== data.account) {
         let num = state.nearMsg[indexInNear]['noViewMsgNum'] + 1;
         state.nearMsg.splice(indexInNear, 1, {
           id: data.fromId,
           headUrl: data.headUrl,
           name: data.name,
-          noViewMsgNum: num
+          noViewMsgNum: num,
+          account: data.account
         });
       }
+      if(obj === undefined){
+        obj = state.msgWith[data.account] = [];
+      }
+      obj.push(data);
+      deleteMoreMsg(obj, state.historyNum);
+      bus.$emit('update');
+
     },
-    addOneToOneMsg(state, {id, name, headUrl, msg}){
-      let obj = state.msgWith[id],
-          indexInNear = state.nearMsg.findIndex(item => item.id === id);
+    addOneToOneMsg(state, {id, name, headUrl, account, msg}){
+      let obj = state.msgWith[account],
+          indexInNear = state.nearMsg.findIndex(item => item.account === account);
       if(indexInNear === -1){
         state.nearMsg.push({
           id,
           headUrl,
           name,
-          noViewMsgNum: 0
+          noViewMsgNum: 0,
+          account
         });
       }
       if(obj === undefined){
-        obj = state.msgWith[id] = [];
+        obj = state.msgWith[account] = [];
       }
       obj.push(msg);
+      deleteMoreMsg(obj, state.historyNum);
+    },
+    setAccount(state, a){
+      state.account = a;
     }
   },
   getters: {
   }
-})
+});
+
+function deleteMoreMsg(arr, maxNum){
+  if(arr.length <= maxNum) return;
+  arr.splice(0, arr.length - maxNum);
+}
